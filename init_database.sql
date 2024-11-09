3affc848ca73dba33735f78498ad87c4ef313019
DartDKU <an301@duke.edu>
2024-11-09 23:03:42 +0800

    Removed additions because it was too complicated 😖

--- init_database.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE sports_complex_db;
-CREATE USER 'djangouser'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON sports_complex_db.* TO 'djangouser'@'localhost';
-FLUSH PRIVILEGES;
