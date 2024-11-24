658af8757ed214e214b09d5be448a5bdebc6ebc2
DartDKU <an301@duke.edu>
2024-11-25 02:18:23 +0800

    Following the ER diagram, created new_schema.sql

---
+++ queries/new_schema.sql
@@ -0,0 +1,125 @@
+DROP DATABASE IF EXISTS sports;
+CREATE DATABASE sports;
+USE sports;
+
+CREATE TABLE user (
+    user_id 		INT PRIMARY KEY AUTO_INCREMENT,
+	netid 			VARCHAR(10) UNIQUE,
+    first_name 		VARCHAR(50) NOT NULL,
+    last_name 		VARCHAR(50),
+    gender			ENUM('male', 'female', 'other', 'undisclosed'),
+    birthday 		DATE, -- Age will be dynamically calculated in queries since it's a derived attribute.
+    email 			VARCHAR(100),
+    telephone	 	BIGINT UNIQUE CHECK(length(phone)<= 15),
+    emergency_contact VARCHAR(255), -- Emergency contact can simply be a long varchar or text since only real humans will read it anyways.
+    member_status 	BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE membership (
+    membership_id 	INT PRIMARY KEY AUTO_INCREMENT,
+    user_id 		INT,
+    start_date 		DATE,
+    end_date 		DATE,
+    state 			ENUM ('active', 'expired', 'pending', 'suspended'),
+    admin_notes 	VARCHAR(200),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE penalty (
+    penalty_id 	INT PRIMARY KEY AUTO_INCREMENT,
+    user_id 	INT,
+    fee 		DECIMAL(10, 2),
+    reason 		TEXT,
+    issued_date DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE sport (
+    sport_id 	INT PRIMARY KEY AUTO_INCREMENT,
+    name 		VARCHAR(200)
+);
+
+CREATE TABLE subscription (
+    subscription_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id         INT NOT NULL,
+    sport_id        INT NOT NULL,
+    sub_date 		DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (sport_id) REFERENCES sport(sport_id)
+);
+
+CREATE TABLE instructor (
+    instructor_id 	INT PRIMARY KEY AUTO_INCREMENT,
+    name 			VARCHAR(100),
+    contact_info 	VARCHAR(255)
+);
+
+CREATE TABLE class (
+    class_id 		INT PRIMARY KEY AUTO_INCREMENT,
+    sport_id 		INT,
+    instructor_id 	INT NOT NULL,
+    schedule 		TEXT, -- Maybe we can expand this into a multivariable attribute with timestamp values for each day of the week?
+    FOREIGN KEY (sport_id) REFERENCES sport(sport_id),
+    FOREIGN KEY (instructor_id) REFERENCES instructor(instructor_id)
+);
+
+CREATE TABLE facility (
+    facility_id INT PRIMARY KEY AUTO_INCREMENT,
+    name 		VARCHAR(100) NOT NULL,
+    reservable	BOOLEAN NOT NULL,
+    floor	 	INT NOT NULL
+);
+
+CREATE TABLE reservation (
+    reservation_id 	INT PRIMARY KEY AUTO_INCREMENT,
+    user_id 		INT,
+    facility_id 	INT,
+    reserve_date 	DATE,
+    time_slot 		VARCHAR(50), -- How can we constrain this to one hour and check for time conflicts?
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (facility_id) REFERENCES facility(facility_id)
+);
+
+CREATE TABLE equipment (
+    equipment_id INT PRIMARY KEY AUTO_INCREMENT,
+    name 		 VARCHAR(100) NOT NULL,
+    number		 INT CHECK (number >= 0), -- i.e. Ball #2, Ball #6, Bike #3
+    admin_note	 TEXT,
+    sport_id 	 INT,
+    FOREIGN KEY (sport_id) REFERENCES sport(sport_id)
+);
+
+CREATE TABLE borrowed (
+    borrow_id 		INT PRIMARY KEY AUTO_INCREMENT,
+    user_id 		INT,
+    equipment_id 	INT,
+    borrow_date 	DATETIME DEFAULT CURRENT_TIMESTAMP,
+    return_date 	DATETIME DEFAULT CURRENT_TIMESTAMP,
+    last_returned	DATETIME,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id)
+);
+
+CREATE TABLE notification (
+    notification_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id 		INT,
+    message 		TEXT,
+    timestamp 		DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE announcement (
+    announcement_id INT PRIMARY KEY AUTO_INCREMENT,
+    sport_id 		INT,
+    message 		TEXT,
+    timestamp 		DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sport_id) REFERENCES sport(sport_id)
+);
+
+CREATE TABLE enrollment (
+    enrollment_id 	INT PRIMARY KEY AUTO_INCREMENT,
+    user_id 		INT,
+    class_id 		INT,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (class_id) REFERENCES class(class_id)
+);
