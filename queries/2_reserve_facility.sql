df73a363cf74a4c8ba459e7c3ee13fca32e198ec
Anar.N <an301@duke.edu>
2024-12-05 16:03:53 +0800

    limit res to 1 hour.

--- queries/2_reserve_facility.sql
+++ queries/2_reserve_facility.sql
@@ -9,7 +9,12 @@ CREATE PROCEDURE reserve_facility(
 )
 BEGIN
     DECLARE conflict_count INT;
+    DECLARE reservation_duration INT;
 
+    -- Calculate reservation duration in minutes
+    SET reservation_duration = TIME_TO_SEC(p_end_time) / 60 - TIME_TO_SEC(p_start_time) / 60;
+
+    -- Check for time conflicts
     SELECT COUNT(*) INTO conflict_count
     FROM reservation
     WHERE facility_id = p_facility_id
@@ -23,6 +28,9 @@ BEGIN
     IF conflict_count > 0 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Reservation time conflict detected.';
+    ELSEIF reservation_duration > 60 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Reservation duration exceeds one hour.';
     ELSE
         INSERT INTO reservation (user_id, facility_id, reserve_date, start_time, end_time)
         VALUES (p_user_id, p_facility_id, p_reserve_date, p_start_time, p_end_time);
